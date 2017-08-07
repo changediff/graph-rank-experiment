6e820bd2ea17c66716bc3bc62d60a8782f11c9fc
Chang.YC <carlsplace@live.cn>
2017-08-07 22:03:25 +0800

    统一变量命名规则, dir代表文件夹路径

--- ke_main.py
+++ ke_main.py
@@ -10,30 +10,30 @@ def evaluate_extraction(dataset, method_name, topn=5, ngrams=2, damping=0.85, om
                         alter_edge = None, alter_node = None):
     """评价实验结果"""
     if dataset == 'kdd':
-        abstr_path = './data/embedding/KDD/abstracts/'
-        out_path = './result/embedding/'
-        gold_path = './data/embedding/KDD/gold/'
-        pr_path = './data/embedding/KDD/pr/'
-        edge_path = './data/embedding/KDD/edge_features/'
-        node_path = './data/embedding/KDD/node_features/'
+        abstr_dir = './data/embedding/KDD/abstracts/'
+        out_dir = './result/embedding/'
+        gold_dir = './data/embedding/KDD/gold/'
+        pr_dir = './data/embedding/KDD/pr/'
+        edge_dir = './data/embedding/KDD/edge_features/'
+        node_dir = './data/embedding/KDD/node_features/'
         file_names = read_file('./data/embedding/KDD/abstract_list').split(',')
     elif dataset == 'www':
-        abstr_path = './data/embedding/WWW/abstracts/'
-        out_path = './result/embedding/'
-        gold_path = './data/embedding/WWW/gold/'
-        pr_path = './data/embedding/WWW/pr/'
-        edge_path = './data/embedding/WWW/edge_features/'
-        node_path = './data/embedding/WWW/node_features/'
+        abstr_dir = './data/embedding/WWW/abstracts/'
+        out_dir = './result/embedding/'
+        gold_dir = './data/embedding/WWW/gold/'
+        pr_dir = './data/embedding/WWW/pr/'
+        edge_dir = './data/embedding/WWW/edge_features/'
+        node_dir = './data/embedding/WWW/node_features/'
         file_names = read_file('./data/embedding/WWW/abstract_list').split(',')
     else:
         print('wrong dataset name')
-    if not os.path.exists(out_path):
-        os.makedirs(out_path)
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
     
     if alter_edge:
-        edge_path = alter_edge
+        edge_dir = alter_edge
     if alter_node:
-        node_path = alter_node
+        node_dir = alter_node
 
     count = 0
     gold_count = 0
@@ -43,10 +43,10 @@ def evaluate_extraction(dataset, method_name, topn=5, ngrams=2, damping=0.85, om
     recall_micro = 0
     for file_name in file_names:
 
-        pr, graph = wpr(edge_path+file_name, node_path+file_name, omega=omega, phi=phi)
+        pr, graph = wpr(edge_dir+file_name, node_dir+file_name, omega=omega, phi=phi)
 
-        gold = read_file(gold_path+file_name)
-        keyphrases = get_phrases(pr, graph, abstr_path, file_name, ng=ngrams)
+        gold = read_file(gold_dir+file_name)
+        keyphrases = get_phrases(pr, graph, abstr_dir, file_name, ng=ngrams)
         top_phrases = []
         for phrase in keyphrases:
             if phrase[0] not in str(top_phrases):
@@ -75,7 +75,7 @@ def evaluate_extraction(dataset, method_name, topn=5, ngrams=2, damping=0.85, om
         # recall_single = count_micro / len(golds)
         # output_single = str(file_name) + ',' + str(prcs_single) + ',' + str(recall_single) + ','\
         #               + ','.join(phrase for phrase in top_phrases) + '\n'
-        # with open(out_path + dataset + 'DETAILS.csv', mode='a', encoding='utf8') as f:
+        # with open(out_dir + dataset + 'DETAILS.csv', mode='a', encoding='utf8') as f:
         #     f.write(output_single)
     prcs = count / extract_count
     recall = count / gold_count
@@ -88,7 +88,7 @@ def evaluate_extraction(dataset, method_name, topn=5, ngrams=2, damping=0.85, om
 
     tofile_result = method_name + ',' + str(prcs) + ',' + str(recall) + ',' + str(f1) + ',' + str(mrr) + ',' \
                     + str(prcs_micro) + ',' + str(recall_micro) + ',' + str(f1_micro) + ',\n'
-    with open(out_path + dataset + 'RESULTS.csv', mode='a', encoding='utf8') as f:
+    with open(out_dir + dataset + 'RESULTS.csv', mode='a', encoding='utf8') as f:
         f.write(tofile_result)
 
 if __name__ == "__main__":
