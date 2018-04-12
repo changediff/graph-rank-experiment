b60406c61a7a01df430b762697c80c296f863b88
Chang.YC <chang.1123@outlook.com>
2018-04-12 13:50:20 +0800

    filter dataset

--- dataset_analyse.py
+++ dataset_analyse.py
@@ -16,41 +16,31 @@ for d in datasetlist:
     cited_dir = cfg.get('dataset', 'cited')
     citing_dir = cfg.get('dataset', 'citing')
     with_tag = cfg.getboolean('dataset', 'with_tag')
-
-    dirlist = [abstract_dir, cited_dir, citing_dir]
-    if not with_tag:
-        for dir in dirlist:
-            corpus += [os.path.join(dir, name) for name in os.listdir(dir)
-                       if os.path.isfile(os.path.join(dir, name))]
-    else:
-        for dir in dirlist:
-            corpus_tagged += [os.path.join(dir, name) for name in os.listdir(dir)
-                              if os.path.isfile(os.path.join(dir, name))]
-aggratedtxt = ''
-for c in corpus:
-    aggratedtxt += read_file(c)
-for c in corpus_tagged:
-    aggratedtxt += rm_tags(read_file(c))
-
-for d in datasetlist:
-    cfg = ConfigParser()
-    cfg.read(os.path.join("./config", d+'.ini'))
     gold_dir = cfg.get('dataset', 'gold')
 
     names = [name for name in os.listdir(gold_dir)
              if os.path.isfile(os.path.join(gold_dir, name))]
-    gold_phrase = []
-    gold_word = []
+    phraseindoc_num = dict()
     for name in names:
-        gold = read_file(os.path.join(gold_dir, name))
-        gold_phrase += [p for p in gold.split('\n') if p != '']
-        gold_word += gold.split()
-    phrase_in_corpus = 0
-    word_in_corpus = 0
-    for p in gold_phrase:
-        if p in aggratedtxt:
-            phrase_in_corpus += 1
-    for w in gold_word:
-        if w in aggratedtxt:
-            word_in_corpus += 1
-    print(d, len(gold_phrase), phrase_in_corpus, len(gold_word), word_in_corpus)
+        gold = read_file(os.path.join(gold_dir, name)).split('\n')
+        if not with_tag:
+            doc = read_file(os.path.join(abstract_dir, name))
+        else:
+            doc = rm_tags(read_file(os.path.join(abstract_dir, name)))
+        if gold[-1] == '':
+            gold = gold[:-1]
+        for g in gold:
+            if g.lower() in doc.lower():
+                phraseindoc_num[name] = phraseindoc_num.get(name, 0) + 1
+    phraseindoc_1 = [key for key, value in phraseindoc_num.items() if value > 0]
+    phraseindoc_2 = [key for key, value in phraseindoc_num.items() if value > 1]
+    phraseindoc_3 = [key for key, value in phraseindoc_num.items() if value > 2]
+
+    for file in ['1phraseindoc','2phraseindoc','3phraseindoc']:
+        with open(os.path.join('./data', d.upper(), file), 'w', encoding='utf8') as f:
+            if '1' in file:
+                f.write('\n'.join(phraseindoc_1))
+            if '2' in file:
+                f.write('\n'.join(phraseindoc_2))
+            if '3' in file:
+                f.write('\n'.join(phraseindoc_3))
